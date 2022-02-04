a3cd29658168bee4e8ddae117efb562d904cffdf
Prasant <prasantraj0808@gmail.com>
2022-02-04 11:14:21 +0530

    first commit : create the directory structure and setup expresss server

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+
+const app=express();
+const port=8000;
+
+app.listen(port,function(err){
+    if(err)
+    {
+        //console.log('error in running the server',error);   //without interpolation
+        console.log(`error in running the server: ${err}`);
+    }
+    else
+    {
+        console.log(`server is running on port : ${port}`);
+    }
+});
