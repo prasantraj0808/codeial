4f75ae95cb6e733376369c228c901ea28a137cb5
Prasant <prasantraj0808@gmail.com>
2022-02-06 14:04:54 +0530

    creating forms for signup and signin

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,30 @@
 module.exports.profile=function(req,res){
     res.end('<h1>user profile</h1>')
 };
+
+//render signup page
+
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title:"codeial| sign up"
+    });
+
+};
+//render sign in page
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title:"codeial| sign in"
+    });
+
+};
+//get the signup data
+module.exports.create=function(req,res){
+    //to do later
+};
+
+//sigin and create the session for the usser
+module.exports.createSession=function(req,res){
+    //to do later
+};
+
